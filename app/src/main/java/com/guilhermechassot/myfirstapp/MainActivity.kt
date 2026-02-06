40a6cec8608b7c6254142f3cb86d690fc2935161
guilhermechassot <guilhermechassotdev@hotmail.com>
2026-02-06 20:47:06 +0000

    Acao do botao

---
+++ app/src/main/java/com/guilhermechassot/myfirstapp/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.guilhermechassot.myfirstapp
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+
+        }
+
+        val edtWeight = findViewById<TextInputEditText>(R.id.edtweight)
+        val edtHeight = findViewById<TextInputEditText>(R.id.edtheight)
+        val BtnCalculate = findViewById<Button>(R.id.btnCalculate)
+
+        BtnCalculate.setOnClickListener {
+            val weight: Float = edtWeight.text.toString().toFloat()
+            val height: Float = edtHeight.text.toString().toFloat()
+
+            val height2 = (height * height)
+            val resultado = weight / height2
+            
+            println("Gui acao do botao" + resultado)
+
+
+
+        }
+
+
+    }
+}
